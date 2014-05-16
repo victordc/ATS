62f68628bf6b997c36b3cdc98bfd5547f87806b9
Kasireddy Vinay <vinaykasireddy@gmail.com>
2014-05-17 07:01:19 +0800

    Added Email Function

--- Shared/ATS.Framework/EmailManager.cs
+++ Shared/ATS.Framework/EmailManager.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Helpers;
+using System.Web.Configuration;
 
 namespace ATS.Framework
 {
     public class EmailManager
     {
+        public int SendMail(string fromEmail, string toEmail, string subject, string body, string email)
+        {
+            try
+            {
+                WebMail.SmtpServer = WebConfigurationManager.AppSettings["SMTPServer"].ToString();
+                WebMail.From = fromEmail;
+                WebMail.Send(
+                        toEmail,
+                        subject,
+                        body,
+                        email
+                    );
+
+                return 1;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
     }
 }
