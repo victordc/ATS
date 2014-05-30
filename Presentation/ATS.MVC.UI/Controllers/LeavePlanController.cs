1a2891e616b5b0e9e11cc5372a11a485e189e388
ghasseminia <ghasseminia@gmail.com>
2014-05-30 12:06:03 +0800

    deleteConfirmed is ajax based now. it returns bool

--- Presentation/ATS.MVC.UI/Controllers/LeavePlanController.cs
+++ Presentation/ATS.MVC.UI/Controllers/LeavePlanController.cs
@@ -19,8 +19,8 @@ namespace ATS.MVC.UI.Controllers
         // GET: /LeavePlan/
         public ActionResult Index()
         {
-            int currentUserId = UserSetting.Current.UserId;
-            //int currentUserId = 1;
+            //int currentUserId = UserSetting.Current.UserId;
+            int currentUserId = 1;
             var leavePlans = TimesheetRepository.GetLeavePlans(currentUserId);
             return View(leavePlans.ToList());
         }
@@ -134,16 +134,22 @@ namespace ATS.MVC.UI.Controllers
             {
                 return false;
             }
-            
-            
+
+
         }
 
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    TimesheetRepository.RemoveLeavePlan(id);
+        //    return RedirectToAction("Index");
+        //}
+
         [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public bool DeleteConfirmed(int id)
         {
-            TimesheetRepository.RemoveLeavePlan(id);
-            return RedirectToAction("Index");
+            return TimesheetRepository.RemoveLeavePlan(id);
         }
 
         protected override void Dispose(bool disposing)
