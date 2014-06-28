644e75cf798aeb532b0791a52a199188cd54825a
victordc <a0120578@nus.edu.sg>
2014-06-28 13:55:13 +0800

    edit page ajax is done.

--- Presentation/ATS.MVC.UI/Controllers/LeavePlanController.cs
+++ Presentation/ATS.MVC.UI/Controllers/LeavePlanController.cs
@@ -23,7 +23,7 @@ namespace ATS.MVC.UI.Controllers
             ViewBag.UserRole = UserSetting.Current.RoleName;
             //int currentUserId = 1;
             var leavePlans = TimesheetRepository.GetLeavePlans(currentUserId);
-            
+
             return View(leavePlans.ToList());
         }
 
@@ -57,10 +57,6 @@ namespace ATS.MVC.UI.Controllers
         [HttpPost]
         public string Create(LeavePlan leaveplan)
         {
-            DateTime start = new DateTime(leaveplan.StartDate.Year, leaveplan.StartDate.Month, leaveplan.StartDate.Day);
-            DateTime end = new DateTime(leaveplan.EndDate.Year, leaveplan.EndDate.Month, leaveplan.EndDate.Day);
-            DateTime today = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
-
             if (leaveplan.StartDate < DateTime.Now || leaveplan.EndDate < DateTime.Now)
             {
                 return "Start and End Dates can not be in the past. Please correct.";
@@ -93,6 +89,7 @@ namespace ATS.MVC.UI.Controllers
         public ActionResult Edit(int id = 0)
         {
             LeavePlan leaveplan = TimesheetRepository.GetLeavePlanById(id);
+            
             if (leaveplan == null)
             {
                 return HttpNotFound();
